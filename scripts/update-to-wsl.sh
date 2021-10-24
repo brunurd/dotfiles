3d580ad2b44cf1d76a0b93272e66f27e8dbcda33
Bruno Araujo <araujo.contato@gmail.com>
2021-10-24 19:46:06 -0300

    fix: .bashrc handling

--- scripts/update-to-wsl.sh
+++ scripts/update-to-wsl.sh
@@ -4,8 +4,10 @@ proj_home="$(dirname $0)/.."
 
 nvim_conf="$proj_home/.config/nvim/init.vim"
 tmux_conf="$proj_home/.tmux.conf"
+bash_conf="$proj_home/.bashrc"
 
 test -d "$HOME/.config/nvim" || mkdir -p "$HOME/.config/nvim"
 
 cp -v $nvim_conf "$HOME/.config/nvim/init.vim"
 cp -v $tmux_conf "$HOME/.tmux.conf"
+cp -v $bash_conf "$HOME/.bashrc"
